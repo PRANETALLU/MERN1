bf864376233b728972213cc08aee5368590496e1
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-05-13 13:35:10 -0400

    login page setup

--- frontend/my-app/src/pages/Login.js
+++ frontend/my-app/src/pages/Login.js
@@ -1,9 +1,37 @@
 import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/material';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async(e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="Login">
-            <Typography>Login</Typography>
+            <Stack direction="column" alignItems="center">
+                <form onSubmit={login}> 
+                <Typography sx={{fontWeight: "bold", marginTop: 3}}>Login</Typography>
+                <TextField 
+                    label="Username" 
+                    variant="outlined" 
+                    sx={{marginTop: 3}}
+                    onChange = {e => setUsername(e.target.value)}
+                />
+                <TextField 
+                    label="Password" 
+                    variant="outlined" 
+                    sx={{marginTop: 3}}
+                    onChange = {e => setPassword(e.target.value)}
+                />
+                <Button variant="contained" sx={{marginTop: 3}}>Login</Button>
+                </form>
+            </Stack>
         </div>
     )
 }
